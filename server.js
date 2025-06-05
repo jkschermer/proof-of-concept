1716b9ec3cb406d77bc88a0c971457a37e88d680
Akikosophia <asschermer@hotmail.com>
2025-06-05 10:59:49 +0200

    #7 Project setup
    Express server opgezet
    
    Liquid als view engine toegevoegd
    
    API gekoppeld en data opgehaald
    
    .env bestand toegevoegd voor API-key
    
    Pagina toont data via users: data

--- server.js
+++ server.js
@@ -1,1 +1,53 @@
-console.log('Hier komt je server voor Sprint 12.')
+import 'dotenv/config'; 
+// laad env bestand
+
+import express from 'express'
+
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('view engine', 'liquid');
+
+app.set('views', './views')
+
+app.get('/mensen-pagina', async (req, res) => {
+
+try {
+    const response = await fetch ('https://the-sprint-api.onrender.com/people', {
+        headers: {
+            'X-API-Key': `${process.env.API_KEY}`
+        }
+    
+});
+
+const data = await response.json();
+console.log(data)
+
+    res.render('mensen-pagina', { users:data}); 
+
+} catch (err) {
+    console.error('Fout bij ophalen:', err);
+    res.status(500).send('Fout bij het ophalen van data');
+  }
+  
+});
+
+app.get('/', async function (request, response) {
+
+    response.render('index.liquid', { });
+  });
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8022)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ hoi`)
+})
