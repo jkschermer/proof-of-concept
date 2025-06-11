0db2fabc7c33570780a2f60d0c0ae5a00c343c5d
Akikosophia <asschermer@hotmail.com>
2025-06-11 20:41:02 +0200

    #11 Tweede API endpoint opgehaald
    Tweede API endpoint opgehaald met Promise all en code netter geformatteerd

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
-import 'dotenv/config'; 
+import "dotenv/config";
 // laad env bestand
 
-import express from 'express'
+import express from "express";
 
-import { Liquid } from 'liquidjs';
+import { Liquid } from "liquidjs";
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 const engine = new Liquid();
-app.engine('liquid', engine.express());
-app.set('view engine', 'liquid');
-app.set('views', './views')
+app.engine("liquid", engine.express());
+app.set("view engine", "liquid");
+app.set("views", "./views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/mensen-pagina', async (req, res) => {
-
-try {
-    const response = await fetch ('https://the-sprint-api.onrender.com/people', {
+app.get("/mensen-pagina", async (req, res) => {
+  try {
+    const [peopleResponse, messagesResponse] = await Promise.all([
+      fetch("https://the-sprint-api.onrender.com/people", {
         headers: {
-            'X-API-Key': `${process.env.API_KEY}`
-        }
-    
-});
+          "X-API-Key": `${process.env.API_KEY}`,
+        },
+      }),
+      fetch("https://fdnd.directus.app/items/messages"),
+    ]);
 
-const data = await response.json();
-console.log(data)
+    const messagesData = await messagesResponse.json();
+    const peopleData = await peopleResponse.json();
 
-    res.render('mensen-pagina', { users:data}); 
+    console.log(messagesData);
 
-} catch (err) {
-    console.error('Fout bij ophalen:', err);
-    res.status(500).send('Fout bij het ophalen van data');
+    res.render("mensen-pagina", { 
+      users: peopleData, 
+      messages: messagesData });
+
+  } catch (err) {
+    console.error("Fout bij ophalen:", err);
+    res.status(500).send("Fout bij het ophalen van data");
   }
-  
 });
 
-app.get('/', async function (request, response) {
-
-    response.render('index.liquid', { });
-  });
+app.get("/", async function (request, response) {
+  response.render("index.liquid", {});
+});
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8022)
+app.set("port", process.env.PORT || 8022);
 
 // Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ hoi`)
-})
+  console.log(`Daarna kun je via http://localhost:${app.get("port")}/ hi `);
+});
